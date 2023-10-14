4565007a633bd77f746aff4e099a7fef2f486885
Harshith Rao <harshithrao316@gmail.com>
2023-10-14 09:49:07 +0530

    minify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,22 @@ export default defineNuxtConfig({
   css: ['vuetify/lib/styles/main.sass', '@/assets/css/styles.css', "@mdi/font/css/materialdesignicons.min.css"],
   build: {
     transpile: ['vuetify'],
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeEmptyElements: true
+      }
+    }
   },
   vite: {
     define: {
